8726dbe9b7fb0cbdaf23969467756d555de80d21
Judah Hunger <judah.hunger@gmail.com>
2018-03-21 19:38:56 -0700

    solved insert-dashes kata

---
+++ insert-dashes/solution.js
@@ -0,0 +1,15 @@
+'use strict'
+function insertDash(num) {
+  num = num.toString();
+  let newString = [];
+  for( let i = 0; i < num.length; i ++){
+   if ((num.charAt(i -1)%2 !== 0)&&(num.charAt(i)%2 !== 0)){
+     newString.push('-', num[i]);
+   }else{
+     newString.push(num[i]);
+   }
+    
+  }
+  return newString.join('');
+}
+
